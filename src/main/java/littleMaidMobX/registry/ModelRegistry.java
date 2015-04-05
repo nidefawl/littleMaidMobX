4d2e06d941f8c6b11ebe10f4c58a6da4a162f863
nidefawl <nidefawl@craftland.org>
2015-04-06 01:29:59 +0200

    Enable all models again

--- src/main/java/littleMaidMobX/registry/ModelRegistry.java
+++ src/main/java/littleMaidMobX/registry/ModelRegistry.java
@@ -7,28 +7,28 @@ import littleMaidMobX.model.maids.*;
 public class ModelRegistry {
 	public static final HashSet<Class> list = new HashSet<Class>();
 	static {
-//		list.add(ModelLittleMaid_AC.class);
+		list.add(ModelLittleMaid_AC.class);
 		list.add(ModelLittleMaid_Orign.class);
-//		list.add(ModelLittleMaid_Archetype.class);
-//		list.add(ModelLittleMaid_Aug.class);
-//		list.add(ModelLittleMaid_RX0.class);
-//		list.add(ModelLittleMaid_SR2.class);
-//		list.add(ModelMulti_Stef.class);
-//		list.add(ModelMulti_Steve.class);
+		list.add(ModelLittleMaid_Archetype.class);
+		list.add(ModelLittleMaid_Aug.class);
+		list.add(ModelLittleMaid_RX0.class);
+		list.add(ModelLittleMaid_SR2.class);
+		list.add(ModelMulti_Stef.class);
+		list.add(ModelMulti_Steve.class);
 		list.add(MultiModel_Evelyn3.class);
-//		list.add(MultiModel_Elsie2.class);
-//		list.add(MultiModel_Beverly2.class);
-//		list.add(MultiModel_Beverly3.class);
-//		list.add(MultiModel_Beverly4.class);
-//		list.add(MultiModel_Beverly5.class);
-//		list.add(MultiModel_Beverly6.class);
-//		list.add(MultiModel_bgs.class);
-//		list.add(MultiModel_brs.class);
-//		list.add(MultiModel_ch.class);
+		list.add(MultiModel_Elsie.class);
+		list.add(MultiModel_Beverly2.class);
+		list.add(MultiModel_Beverly3.class);
+		list.add(MultiModel_Beverly4.class);
+		list.add(MultiModel_Beverly5.class);
+		list.add(MultiModel_Beverly6.class);
+		list.add(MultiModel_bgs.class);
+		list.add(MultiModel_brs.class);
+		list.add(MultiModel_ch.class);
 		list.add(MultiModel_ChibiNeko.class);
-//		list.add(MultiModel_chrno.class);
-//		list.add(MultiModel_chu.class);
-//		list.add(MultiModel_dm.class);
+		list.add(MultiModel_chrno.class);
+		list.add(MultiModel_chu.class);
+		list.add(MultiModel_dm.class);
 		list.add(MultiModel_NM.class);
 	}
 
