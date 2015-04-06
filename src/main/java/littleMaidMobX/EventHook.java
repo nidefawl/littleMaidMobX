085d87913681733054151815371ea54e560b0402
nidefawl <nidefawl@craftland.org>
2015-04-06 20:31:48 +0200

    Made it so the damagesource for attacks is DamageSource.causeMobDamage(this.avatar) (with swords). Still drops xp, not sure why

--- src/main/java/littleMaidMobX/EventHook.java
+++ src/main/java/littleMaidMobX/EventHook.java
@@ -1,13 +1,30 @@
 package littleMaidMobX;
 
+import littleMaidMobX.entity.EntityLittleMaid;
 import littleMaidMobX.entity.EntityLittleMaidAvatar;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHook
 {
+	public static boolean isLiving(Entity e) {
+		return e instanceof EntityLiving;
+	}
+	public static boolean isLivingBase(Entity e) {
+		return e instanceof EntityLivingBase;
+	}
+	public static boolean isAvatar(Entity e) {
+		return e instanceof EntityLittleMaidAvatar;
+	}
+	public static boolean isMaid(Entity e) {
+		return e instanceof EntityLittleMaid;
+	}
 	@SubscribeEvent
 	public void onEntityItemPickupEvent(EntityItemPickupEvent event)
 	{
@@ -27,9 +44,19 @@ public class EventHook
 			if (arrow.shootingEntity instanceof EntityLittleMaidAvatar) {
 				EntityLittleMaidAvatar avatar = (EntityLittleMaidAvatar) arrow.shootingEntity;
 				arrow.shootingEntity = avatar.avatar;
-				LittleMaidMobX.Debug("Set "+event.entity.getClass()+" field shootingEntity from avator to maid");
-
 			}
+		}
+	}
+	@SubscribeEvent
+	public void onLivingSetAttacktarget(LivingSetAttackTargetEvent event)
+	{
+		if (isLiving(event.entity) && isAvatar(((EntityLiving) event.entity).getAttackTarget())) {
+			EntityLittleMaidAvatar avatar = (EntityLittleMaidAvatar) event.target;
+			((EntityLiving) event.entity).setAttackTarget(avatar.avatar);
+		}
+		if (isLivingBase(event.entity) && isAvatar(((EntityLivingBase) event.entity).getAITarget())) {
+			EntityLittleMaidAvatar avatar = (EntityLittleMaidAvatar) event.target;
+			((EntityLivingBase) event.entity).setRevengeTarget(avatar.avatar);
 		}
 	}
 }
--- src/main/java/littleMaidMobX/entity/EntityLittleMaidAvatar.java
+++ src/main/java/littleMaidMobX/entity/EntityLittleMaidAvatar.java
@@ -6,21 +6,29 @@ import littleMaidMobX.LittleMaidMobX;
 import littleMaidMobX.Statics;
 import littleMaidMobX.sound.EnumSound;
 import littleMaidMobX.wrapper.MinecraftWrapper;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityMultiPart;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.BaseAttributeMap;
+import net.minecraft.entity.boss.EntityDragonPart;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.stats.AchievementList;
 import net.minecraft.stats.StatBase;
+import net.minecraft.stats.StatList;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 
 
 
@@ -162,6 +170,9 @@ public class EntityLittleMaidAvatar extends EntityPlayer {
 		}
 	}
 
+	// maybe do not attack "as player" at all. not sure where the advantage is, 
+	// enchantments and armor works for EntityLivingBase as well
+	// of course it would not drop items then
 	@Override
 	public void attackTargetEntityWithCurrentItem(Entity par1Entity) {
 		// TODO:
@@ -169,7 +180,8 @@ public class EntityLittleMaidAvatar extends EntityPlayer {
 		if (par1Entity instanceof EntityLivingBase) {
 			ll = ((EntityLivingBase)par1Entity).getHealth();
 		}
-		super.attackTargetEntityWithCurrentItem(par1Entity);
+//		super.attackTargetEntityWithCurrentItem(par1Entity);
+		this.localAttackTargetEntityWithCurrentItem(par1Entity);
 		if (par1Entity instanceof EntityLivingBase) {
 			((EntityLivingBase)par1Entity).setRevengeTarget(avatar);
 		}
@@ -179,6 +191,137 @@ public class EntityLittleMaidAvatar extends EntityPlayer {
 		if (ll > 0) {
 			LittleMaidMobX.Debug(String.format("ID:%d Given Damege:%f", avatar.getEntityId(), ll - ((EntityLivingBase)par1Entity).getHealth()));
 		}
+	}
+
+	private void localAttackTargetEntityWithCurrentItem(Entity p_71059_1_) {
+
+		//do not fire "player attacks entity" event
+//        if (MinecraftForge.EVENT_BUS.post(new AttackEntityEvent(this, p_71059_1_))) 
+//        {
+//            return;
+//        }
+        ItemStack stack = getCurrentEquippedItem();
+     // maybe remove this too, it passes our player entity along
+        if (stack != null && stack.getItem().onLeftClickEntity(stack, this, p_71059_1_)) 
+        {
+            return;
+        }
+        if (p_71059_1_.canAttackWithItem())
+        {
+            if (!p_71059_1_.hitByEntity(this.avatar))
+            {
+                float f = (float)this.getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue();
+                int i = 0;
+                float f1 = 0.0F;
+
+                if (p_71059_1_ instanceof EntityLivingBase)
+                {
+                    f1 = EnchantmentHelper.getEnchantmentModifierLiving(this, (EntityLivingBase)p_71059_1_);
+                    i += EnchantmentHelper.getKnockbackModifier(this, (EntityLivingBase)p_71059_1_);
+                }
+
+                if (this.isSprinting())
+                {
+                    ++i;
+                }
+
+                if (f > 0.0F || f1 > 0.0F)
+                {
+                    boolean flag = this.fallDistance > 0.0F && !this.onGround && !this.isOnLadder() && !this.isInWater() && !this.isPotionActive(Potion.blindness) && this.ridingEntity == null && p_71059_1_ instanceof EntityLivingBase;
+
+                    if (flag && f > 0.0F)
+                    {
+                        f *= 1.5F;
+                    }
+
+                    f += f1;
+                    boolean flag1 = false;
+                    int j = EnchantmentHelper.getFireAspectModifier(this);
+
+                    if (p_71059_1_ instanceof EntityLivingBase && j > 0 && !p_71059_1_.isBurning())
+                    {
+                        flag1 = true;
+                        p_71059_1_.setFire(1);
+                    }
+
+//                    boolean flag2 = p_71059_1_.attackEntityFrom(DamageSource.causePlayerDamage(this), f);
+                    boolean flag2 = p_71059_1_.attackEntityFrom(DamageSource.causeMobDamage(this.avatar), f);
+
+                    if (flag2)
+                    {
+                        if (i > 0)
+                        {
+                            p_71059_1_.addVelocity((double)(-MathHelper.sin(this.rotationYaw * (float)Math.PI / 180.0F) * (float)i * 0.5F), 0.1D, (double)(MathHelper.cos(this.rotationYaw * (float)Math.PI / 180.0F) * (float)i * 0.5F));
+                            this.motionX *= 0.6D;
+                            this.motionZ *= 0.6D;
+                            this.setSprinting(false);
+                        }
+
+                        if (flag)
+                        {
+                            this.onCriticalHit(p_71059_1_);
+                        }
+
+                        if (f1 > 0.0F)
+                        {
+                            this.onEnchantmentCritical(p_71059_1_);
+                        }
+
+                        if (f >= 18.0F)
+                        {
+                            this.triggerAchievement(AchievementList.overkill);
+                        }
+
+                        this.setLastAttacker(p_71059_1_);
+
+                        if (p_71059_1_ instanceof EntityLivingBase)
+                        {
+                            EnchantmentHelper.func_151384_a((EntityLivingBase)p_71059_1_, this);
+                        }
+
+                        EnchantmentHelper.func_151385_b(this, p_71059_1_);
+                        ItemStack itemstack = this.getCurrentEquippedItem();
+                        Object object = p_71059_1_;
+
+                        if (p_71059_1_ instanceof EntityDragonPart)
+                        {
+                            IEntityMultiPart ientitymultipart = ((EntityDragonPart)p_71059_1_).entityDragonObj;
+
+                            if (ientitymultipart != null && ientitymultipart instanceof EntityLivingBase)
+                            {
+                                object = (EntityLivingBase)ientitymultipart;
+                            }
+                        }
+
+                        if (itemstack != null && object instanceof EntityLivingBase)
+                        {
+                            itemstack.hitEntity((EntityLivingBase)object, this);
+
+                            if (itemstack.stackSize <= 0)
+                            {
+                                this.destroyCurrentEquippedItem();
+                            }
+                        }
+
+                        if (p_71059_1_ instanceof EntityLivingBase)
+                        {
+                            this.addStat(StatList.damageDealtStat, Math.round(f * 10.0F));
+
+                            if (j > 0)
+                            {
+                                p_71059_1_.setFire(j * 4);
+                            }
+                        }
+
+                        this.addExhaustion(0.3F);
+                    }
+                    else if (flag1)
+                    {
+                        p_71059_1_.extinguish();
+                    }
+                }
+            }
+        }
 	}
 
 	@Override
@@ -567,4 +710,5 @@ public class EntityLittleMaidAvatar extends EntityPlayer {
 	{
 		super.damageEntity(par1DamageSource, par2);
 	}
+	
 }
