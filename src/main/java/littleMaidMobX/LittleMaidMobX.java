03aa70f1ab65b605d0bb5edd1c4a96f504b5d94e
Legojer <legojr33@gmail.com>
2015-12-26 22:16:01 -0600

    Update 1
    Gave the achievement a title and description.
    
    Converted spawning to a biome type based method. It could probably use
    more changes.
    
    Converted the config to Forge's config style.

--- src/main/java/littleMaidMobX/LittleMaidMobX.java
+++ src/main/java/littleMaidMobX/LittleMaidMobX.java
@@ -31,68 +31,42 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.BiomeDictionary;
+import static net.minecraftforge.common.BiomeDictionary.Type;
 
 @Mod(	modid = LittleMaidMobX.DOMAIN,
 		name  = LittleMaidMobX.DOMAIN, version = "2")
+		
 public class LittleMaidMobX {
 	
 	public static final String DOMAIN = "lmmx";
 
-	public static String[] cfg_comment = {
-		"spawnWeight = Relative spawn weight. The lower the less common. 10=pigs. 0=off",
-		"spawnLimit = Maximum spawn count in the World.",
-		"minGroupSize = Minimum spawn group count.",
-		"maxGroupSize = Maximum spawn group count.",
-		"canDespawn = It will despawn, if it lets things go. ",
-		"checkOwnerName = At local, make sure the name of the owner. ",
-		"antiDoppelganger = Not to survive the doppelganger. ",
-		"enableSpawnEgg = Enable LMM SpawnEgg Recipe. ",
-		"VoiceDistortion = LittleMaid Voice distortion.",
-		"defaultTexture = Default selected Texture Packege. Null is Random",
-		"DebugMessage = Print Debug Massages.",
-		"DeathMessage = Print Death Massages.",
-		"Dominant = Spawn Anywhere.",
-		"Aggressive = true: Will be hostile, false: Is a pacifist",
-		"IgnoreItemList = aaa, bbb, ccc: Items little maid to ignore",
-//		"AchievementID = used Achievement index.(0 = Disable)",
-//		"UniqueEntityId = UniqueEntityId(0 is AutoAssigned. max 255)"
-	};
+	public static class Categories
+	{
+        public static final String CATEGORY_MAIDS = "Maids";
+		public static final String CATEGORY_MMMLIB = "MMMLib";
+		public static final String CATEGORY_ITEMS = "Items";
+	}
 	
-//	@MLProp(info="Relative spawn weight. The lower the less common. 10=pigs. 0=off")
-	public static int cfg_spawnWeight = 5;
-//	@MLProp(info="Maximum spawn count in the World.")
-	public static int cfg_spawnLimit = 20;
-//	@MLProp(info="Minimum spawn group count.")
-	public static int cfg_minGroupSize = 1;
-//	@MLProp(info="Maximum spawn group count.")
-	public static int cfg_maxGroupSize = 3;
-//	@MLProp(info="It will despawn, if it lets things go. ")
-	public static boolean cfg_canDespawn = false;
-//	@MLProp(info="At local, make sure the name of the owner. ")
-	public static boolean cfg_checkOwnerName = false;
-//	@MLProp(info="Not to survive the doppelganger. ")
-	public static boolean cfg_antiDoppelganger = true;
-//	@MLProp(info="Enable LMM SpawnEgg Recipe. ")
-	public static boolean cfg_enableSpawnEgg = true;
+	public static int cfg_spawnWeight;
+	public static int cfg_spawnLimit;
+	public static int cfg_minGroupSize;
+	public static int cfg_maxGroupSize;
+	public static boolean cfg_canDespawn;
+	public static boolean cfg_checkOwnerName;
+	public static boolean cfg_antiDoppelganger;
+	public static boolean cfg_enableSpawnEgg;
+	public static boolean cfg_VoiceDistortion;
+	public static String cfg_defaultTexture;
+	public static boolean cfg_DeathMessage;
+	public static boolean cfg_Dominant;
+	public static boolean cfg_Aggressive;
+	public static String cfg_IgnoreItemList;
+	public static boolean cfg_makeNoise;
 	
-	
-//	@MLProp(info="LittleMaid Voice distortion.")
-	public static boolean cfg_VoiceDistortion = true;
-	
-//	@MLProp(info="Default selected Texture Packege. Null is Random")
-	public static String cfg_defaultTexture = "";
-//	@MLProp(info="Print Death Massages.")
-	public static boolean cfg_DeathMessage = true;
-//	@MLProp(info="Spawn Anywhere.")
-	public static boolean cfg_Dominant = false;
-//	@MLProp(info="true: AlphaBlend(request power), false: AlphaTest(more fast)")
-//	public static boolean AlphaBlend = true;
-//	@MLProp(info="true: Will be hostile, false: Is a pacifist")
-	public static boolean cfg_Aggressive = true;
-	public static String cfg_IgnoreItemList = "arsmagica2";
+	public static boolean cfg_isModelAlphaBlend = true;
 
 	public static Achievement ac_Contract;
-	public static boolean cfg_isModelAlphaBlend = true;
 
 	public static boolean isDebugMessage = true;
 	public static boolean isDebugModels = false;
@@ -148,34 +122,54 @@ public class LittleMaidMobX {
 
 	
 	@EventHandler
-	public void PreInit(FMLPreInitializationEvent evt)
+	public void PreInit(FMLPreInitializationEvent event)
 	{
-		{
-
-			File configFile = evt.getSuggestedConfigurationFile();
-			Configuration lconf = new Configuration(configFile);
-			lconf.load();
-			isDebugMessage		= true;//lconf.get("MMMLib", "isDebugMessage", false).getBoolean(false);
-			isModelAlphaBlend	= lconf.get("MMMLib", "isModelAlphaBlend", true).getBoolean(true);
+			Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+			config.load();
+			//MMMLib
+			isDebugMessage		= true;//config.get("MMMLib", "isDebugMessage", false).getBoolean(false);
+			isModelAlphaBlend	= config.getBoolean("Is Model Alpha Blend", Categories.CATEGORY_MMMLIB, true, "true: AlphaBlend(requires more power), false: AlphaTest(faster)");
 			cfg_isModelAlphaBlend = isModelAlphaBlend;
+			//public static boolean AlphaBlend = true;
 			
-			lconf.save();
+			//Maids
+			cfg_spawnWeight = config.getInt("Spawn Weight", Categories.CATEGORY_MAIDS, 3, 0, 10, "Relative spawn weight. The lower the less common. 10=pigs. 0=off");
+			cfg_spawnLimit = config.getInt("Spawn Limit", Categories.CATEGORY_MAIDS, 30, 10, 100, "Maximum spawn count in the World.");
+			cfg_minGroupSize = config.getInt("Minimum Group Size", Categories.CATEGORY_MAIDS, 1, 1, 10, "Minimum spawn group count.");
+			cfg_maxGroupSize = config.getInt("Maximum Group Size", Categories.CATEGORY_MAIDS, 3, 1, 10, "Maximum spawn group count.");
+			cfg_canDespawn = config.getBoolean("Can Despawn", Categories.CATEGORY_MAIDS, false, "It will despawn, if it lets things go.");
+			cfg_checkOwnerName = config.getBoolean("Check Owner Name", Categories.CATEGORY_MAIDS, false, "At local, make sure the name of the owner.");
+			cfg_antiDoppelganger = config.getBoolean("Anti Doppelganger", Categories.CATEGORY_MAIDS, true, "Not to survive the doppelganger.");
+			cfg_VoiceDistortion = config.getBoolean("Voice Distortionion", Categories.CATEGORY_MAIDS, true, "Enables Maid voices to distort based of hair color");
+			cfg_defaultTexture = config.getString("Default Texture", Categories.CATEGORY_MAIDS, "", "Default selected Texture Packege. Null is Random");
+			cfg_DeathMessage = config.getBoolean("Print Death Message", Categories.CATEGORY_MAIDS, true, "Prints message on the death of your maid.");
+			cfg_Aggressive = config.getBoolean("Agressive", Categories.CATEGORY_MAIDS, true, "true: Will be hostile, false: Is a pacifist");
+			cfg_IgnoreItemList = config.getString("Ignore Item List", Categories.CATEGORY_MAIDS, "arsmagica2", "?");
+			//Used to be Spawn Anywhere but I don't know what it does now.
+			cfg_Dominant = false;
+			
+			//Items
+			cfg_enableSpawnEgg = config.getBoolean("Enable Spawn Egg", Categories.CATEGORY_ITEMS, true, "Enables Little Maid SpawnEgg Recipe.");
+
+			config.save();
 			
 			proxy.loadTextures();
 //			ModelManager.instance.loadTextures();
-			if (Helper.isClient) {
+			if (Helper.isClient)
+			{
 //				MMM_TextureManager.loadTextures();
 				Debug("Localmode: InitTextureList.");
 				ModelManager.instance.initTextureList(true);
-			} else {
+			}
+			else
+			{
 				ModelManager.instance.loadTextureServer();
 			}
-		}
-		Config.init();
+		//Config.init();
 		
 		
 //		MMM_Helper.checkRevision("6");
-		Config.checkConfig(this.getClass());
+		//Config.checkConfig(this.getClass());
 		
 		cfg_defaultTexture = cfg_defaultTexture.trim();
 
@@ -238,10 +232,12 @@ public class LittleMaidMobX {
 		
 		ModelManager.instance.setDefaultTexture(EntityLittleMaid.class, ModelManager.instance.getTextureBox("default_Orign"));
 		
-		// Dominant
-		BiomeGenBase[] biomeList = null;
-		if(cfg_spawnWeight > 0) {
-			if (cfg_Dominant)
+		//Determine Biomes to generate in
+		if (cfg_spawnWeight > 0)
+		{
+			BiomeDictionary.registerAllBiomes();
+			BiomeGenBase[] biomeList = null;
+			/*if (cfg_Dominant)
 			{
 				biomeList = BiomeGenBase.getBiomeGenArray();
 			}
@@ -266,18 +262,84 @@ public class LittleMaidMobX {
 					EntityRegistry.addSpawn(EntityLittleMaid.class,
 							cfg_spawnWeight, cfg_minGroupSize, cfg_maxGroupSize, EnumCreatureType.creature, biome);
 				}
+			}*/
+			for(int i = 1; i<=12; i++)
+			{
+				switch (i)
+				{
+					case 1:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.HOT);
+						break;
+					}
+					case 2:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.DRY);
+						break;
+					}
+					case 3:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.SANDY);
+						break;
+					}
+					case 4:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.PLAINS);
+						break;
+					}
+					case 5:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.SAVANNA);
+						break;
+					}
+					case 6:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.SPARSE);
+						break;
+					}
+					case 7:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.MUSHROOM);
+						break;
+					}
+					case 8:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.FOREST);
+						break;
+					}
+					case 9:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.WET);
+						break;
+					}
+					case 10:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.SWAMP);
+						break;
+					}
+					case 11:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.COLD);
+						break;
+					}
+					case 12:
+					{
+						biomeList = BiomeDictionary.getBiomesForType(Type.CONIFEROUS);
+						break;
+					}
+				}
+				for(BiomeGenBase biome : biomeList)
+				{
+					if (biome!=null && (!BiomeDictionary.isBiomeOfType(biome, Type.OCEAN) && !BiomeDictionary.isBiomeOfType(biome, Type.MOUNTAIN) && !BiomeDictionary.isBiomeOfType(biome, Type.HILLS) && !BiomeDictionary.isBiomeOfType(biome, Type.RIVER) && !BiomeDictionary.isBiomeOfType(biome, Type.BEACH) && !BiomeDictionary.isBiomeOfType(biome, Type.END) && !BiomeDictionary.isBiomeOfType(biome, Type.NETHER) && !BiomeDictionary.isBiomeOfType(biome, Type.JUNGLE) && !BiomeDictionary.isBiomeOfType(biome, Type.SNOWY) && !BiomeDictionary.isBiomeOfType(biome, Type.MESA) && !BiomeDictionary.isBiomeOfType(biome, Type.DEAD) && !BiomeDictionary.isBiomeOfType(biome, Type.SPOOKY) && !BiomeDictionary.isBiomeOfType(biome, Type.MAGICAL)))
+					{
+						//Registering Spawn in Biome
+						EntityRegistry.addSpawn(EntityLittleMaid.class, cfg_spawnWeight, cfg_minGroupSize, cfg_maxGroupSize, EnumCreatureType.creature, biome);
+					}
+				}
 			}
 		}
-		
-		
-		
-
-		
-		
 		IFF.loadIFFs();
 	}
-	
-
 	
 	private static String ignoreItemList[] = new String[]{};
 
